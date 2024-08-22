dd71fdafb3780a79923dc7296400b354db640769
Kevin Sean Kho <kkho@ph-cfmb4m3.lrdc.lexmark.com>
2024-08-22 21:08:06 +0800

    removed errors on formatted date

--- tvstartup-ui/src/app/home/home.component.ts
+++ tvstartup-ui/src/app/home/home.component.ts
@@ -1,37 +1,36 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
 import { VideoService } from "../video.service"
 import { VideoList } from '../video-list';
 import { CommonModule } from '@angular/common';
-import { DatePipe } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ NavbarComponent, FooterComponent, CommonModule, DatePipe, RouterModule, RouterOutlet],
+  imports: [ NavbarComponent, FooterComponent, CommonModule, RouterModule, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
 })
 
-export class HomeComponent implements OnInit  
-{
-    title = "Home";
-    videos: VideoList[] = [];
+export class HomeComponent {
+  title = 'Home';
+  videos: VideoList[] = [];
 
-    constructor(private videoService: VideoService, private datePipe:DatePipe) { }
-    
-    ngOnInit(): void {
-      this.videoService.getVideos().subscribe(data => {
-        this.videos = data.map(video => {
-          const formattedVideo = {
-            ...video,
-            formattedDate: this.datePipe.transform(video.date_posted, 'MMM d, y, h:mm a')
-          };
-          return formattedVideo;
-        });
-      });
-    }
+  constructor(private videoService: VideoService) {}
+
+  fetchVideos()
+  {
+    this.videoService.getVideos().subscribe((data: VideoList[]) => {
+      this.videos = data;
+    });
+  }
+
+  ngOnInit(): void
+  {
+    this.fetchVideos();
+  }
 }
