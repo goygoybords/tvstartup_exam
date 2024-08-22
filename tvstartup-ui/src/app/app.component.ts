8cd33db68ad033e0c646c5fb4f193dbf5484dadb
Kevin Sean Kho <kkho@ph-cfmb4m3.lrdc.lexmark.com>
2024-08-22 09:49:40 +0800

    Splice the index file:
    created home, navbar, and footer components

--- tvstartup-ui/src/app/app.component.ts
+++ tvstartup-ui/src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent implements OnInit
 {
+    title = "tvstartup-ui"
     ngOnInit(): void
     {
         const script = document.createElement('script');
