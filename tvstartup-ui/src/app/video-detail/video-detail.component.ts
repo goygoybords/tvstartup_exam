18735bd7e21febcee655cee3ed1e3184e42f4061
Kevin Sean Kho <kkho@ph-cfmb4m3.lrdc.lexmark.com>
2024-08-22 21:35:46 +0800

    View Specific Video partial view

--- tvstartup-ui/src/app/video-detail/video-detail.component.ts
+++ tvstartup-ui/src/app/video-detail/video-detail.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { AfterViewInit } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-video-detail',
@@ -10,6 +13,27 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   templateUrl: './video-detail.component.html',
   styleUrl: './video-detail.component.css'
 })
-export class VideoDetailComponent {
+export class VideoDetailComponent implements  OnInit, AfterViewInit
+{
+  videoId: string | null = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.videoId = params.get('id'); // Access the id parameter
+      console.log("id = " + this.videoId); // Do something with the id
+    });
+  }
+
+
+  ngAfterViewInit()
+  {
+    $('.tm-hero').each(function()
+    {
+        var imageSrc = $(this).attr('data-image-src');
+        if (imageSrc)
+          $(this).css('background-image', 'url(' + imageSrc + ')');
+    });
+  }
 }
